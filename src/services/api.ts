bb3f51d44f63b4eb6e1b42e9e663e0a8a21d832e
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-04-11 13:52:13 -0300

    Primeiro commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-     baseURL: "pizzaria-backend-production-bccd.up.railway.app"
+     baseURL: "https://pizzaria-backend-production-bccd.up.railway.app"
 })
