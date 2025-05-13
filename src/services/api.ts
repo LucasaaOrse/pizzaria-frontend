d19c80034c8cb064bb2c0cb9b3055b51456a1c6d
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-05-13 11:54:29 -0300

    feat: adiciona rotas de criação, ajuste de estoque e listagem de ingredientes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 // src/services/api.ts
 import axios from "axios";
 
-const serverBaseURL = process.env.API_URL;            // para Server Actions
-const clientBaseURL = process.env.NEXT_PUBLIC_API_URL; // para browser
+const serverBaseURL = process.env.API_URL ?? "https://pizzaria-backend-production-bccd.up.railway.app";
+const clientBaseURL = process.env.NEXT_PUBLIC_API_URL ?? "https://pizzaria-backend-production-bccd.up.railway.app";
 
 export const api = axios.create({
   baseURL: typeof window === "undefined" ? serverBaseURL : clientBaseURL,
