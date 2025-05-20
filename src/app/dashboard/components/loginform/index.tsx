528bab66f2d31f764671a1160b7e681c306dc4bc
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-05-20 14:29:10 -0300

    Atualização

--- src/app/dashboard/components/loginform/index.tsx
+++ src/app/dashboard/components/loginform/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useTransition } from "react";
-import styles from "./page.module.scss";
+import styles from "../../../page.module.scss";
 
 export default function LoginForm({
   handleLogin,
