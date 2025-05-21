868e8235a38cb29ac64c63a80190017d0bb23157
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-05-21 11:43:31 -0300

    Atualização

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,10 +1,27 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import "./loading.module.scss";
 
 export default function Loading() {
+  const [dots, setDots] = useState(".");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + "." : "."));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="loading-container">
-      <div className="spinner" />
-      <span>Carregando...</span>
+      <div className="dot-loader">
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+      </div>
+      <span className="loading-text">Carregando{dots}</span>
     </div>
   );
 }
